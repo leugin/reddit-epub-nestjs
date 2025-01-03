2ed5dd87ee1ee81912d17a29336d9dc6b9056284
miguel <meqh1992@gmail.com>
2025-01-02 20:33:54 -0500

    feat: add basic get route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('/api/v1/reddit/find')
+  findAll(
+    @Query('sub_reddit') subReddit: string,
+    @Query('search') search: string,
+    @Res() res: Response,
+  ): any {
+    res.status(HttpStatus.OK).json({ subReddit, search });
   }
 }
