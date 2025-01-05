de54a52799dbfeb803c00ef634f5763409ba8fd0
miguel <meqh1992@gmail.com>
2025-01-05 14:18:58 -0500

    feat: feat add reddit find service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,27 @@
 import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { RedditService } from './provider/reddit/reddit.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly reeditService: RedditService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
   @Get('/api/v1/reddit/find')
-  findAll(
+  async findAll(
     @Query('sub_reddit') subReddit: string,
     @Query('search') search: string,
     @Res() res: Response,
-  ): any {
-    res.status(HttpStatus.OK).json({ subReddit, search });
+  ) {
+    const data = await this.reeditService.findAll(subReddit, search);
+    const json = await data;
+    res.status(HttpStatus.OK).json(json);
   }
 }
